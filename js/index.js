41ae374c1e7559f54a88523f870f590066feb597
drunkeer <43085558+drunkeer@users.noreply.github.com>
2024-06-12 07:59:34 +0800

    Update and rename index.js to js/index.js

--- js/index.js
+++ js/index.js
@@ -429,64 +429,6 @@ option && myChart.setOption(option);
   
    }
   )();
-(
-  function(){
-    var dom = document.getElementById('chart4');
-    var myChart = echarts.init(dom, null, {
-      renderer: 'canvas',
-      useDirtyRect: false
-    });
-    var app = {};
-    // var ROOT_PATH = 'https://echarts.apache.org/examples';
-    var option;
-
-    $.getJSON('flights.json', function (data) {
-  function getAirportCoord(idx) {
-    return [data.airports[idx][3], data.airports[idx][4]];
-  }
-  var routes = data.routes.map(function (airline) {
-    return [getAirportCoord(airline[1]), getAirportCoord(airline[2])];
-  });
-  myChart.setOption({
-    backgroundColor: '#000',
-    globe: {
-      baseTexture: 'img/world.topo.bathy.200401.jpg',
-      heightTexture:
-        'img/bathymetry_bw_composite_4k.jpg',
-      shading: 'lambert',
-      light: {
-        ambient: {
-          intensity: 0.4
-        },
-        main: {
-          intensity: 0.4
-        }
-      },
-      viewControl: {
-        autoRotate: false
-      }
-    },
-    series: {
-      type: 'lines3D',
-      coordinateSystem: 'globe',
-      blendMode: 'lighter',
-      lineStyle: {
-        width: 1,
-        color: 'rgb(50, 50, 150)',
-        opacity: 0.1
-      },
-      data: routes
-    }
-  });
-});
-
-    if (option && typeof option === 'object') {
-      myChart.setOption(option);
-    }
-
-    window.addEventListener('resize', myChart.resize);
-  }
-)();
 
 (function(){
 
